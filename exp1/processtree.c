f417d59d0038000645babd9eeda185e09a9cce38
kirang093 <kirankumarg057@gmail.com>
2023-02-27 14:26:57 +0530

    .

---
+++ exp1/processtree.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+/*
+ * 			Creating this kind of Process Tree.
+ *
+ * 							A
+ *
+ *					B				C
+ *
+ *					D				E
+ *
+*/ 					
+
+
+int main() {
+    pid_t pid1, pid2, pid3, pid4;
+
+    printf("Parent of all PID : %d\n", getpid());   // Say (A)
+
+    pid1 = fork();
+
+    if (pid1 == 0) {
+        printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());   // say (B)
+        pid2 = fork();
+        if (pid2 == 0) {
+            printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());  // say (D)
+        }
+		if(pid2 > 0) wait(NULL);
+    }
+
+    if (pid1 > 0) {
+		wait(NULL);
+        pid3 = fork();
+        if (pid3 == 0) {
+            printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());  // say (C)
+            pid4 = fork();
+            if (pid4 == 0) {
+                printf("Child PID : %d with parent PID : %d\n", getpid(), getppid());  // say (E).
+            }
+			if(pid4 > 0) wait(NULL);
+        }
+		if (pid3 > 0) wait(NULL);
+    }
+
+    return 0;
+}
